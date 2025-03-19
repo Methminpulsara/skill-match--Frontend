85c8c69abb0a0e880f03dc078254c9af47fb94e8
Methminpulsara <methminpulsara10@gmail.com>
2025-03-19 19:39:19 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SingInComponent } from './page/sing-in/sing-in.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [SingInComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './page/login/login.component';
+import { SingInComponent } from './page/sing-in/sing-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"",
+    component: LoginComponent
+  },{
+    path:"login",
+    component: LoginComponent
+  },
+  {
+    path:"singin",
+    component: SingInComponent
+  }
+];
