91c22a02fba365f2c78dea17216dd3714e031af6
Methminpulsara <methminpulsara10@gmail.com>
2025-03-28 21:59:27 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { SingInComponent } from './page/sing-in/sing-in.component';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeProfileComponent } from './page/employee/employee-profile/employee-profile.component';
 import { TheamComponent } from './page/theam/theam.component';
+import { TrainingsComponent } from './page/company/trainings/trainings.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TrainingsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
