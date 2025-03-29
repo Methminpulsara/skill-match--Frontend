4b74ec33bdac701251b9e6a1b9cc7d9baa875765
Methminpulsara <methminpulsara10@gmail.com>
2025-03-29 23:04:20 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SingInComponent } from './page/sing-in/sing-in.component';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeProfileComponent } from './page/employee/employee-profile/employee-profile.component';
@@ -14,10 +14,21 @@ import { InternaliJobBoardComponent } from './page/company/internali-job-board/i
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent   {
-  title = 'web-app';
+export class AppComponent implements OnInit {
+  currentTheme: string = 'light';
 
+  ngOnInit() {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    this.setTheme(savedTheme);
+  }
 
+  toggleTheme(event: any) {
+    this.currentTheme = event.target.checked ? 'dark' : 'light';
+    this.setTheme(this.currentTheme);
+  }
 
-
+  setTheme(theme: string) {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }
 }
