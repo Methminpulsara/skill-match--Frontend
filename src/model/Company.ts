d16e8b4da6d199fbb25b646477b035ca46cd69f0
Methminpulsara <methminpulsara10@gmail.com>
2025-03-31 20:27:42 +0530

    update

--- src/model/Company.ts
+++ src/model/Company.ts
@@ -1,4 +1,27 @@
 export default class className  {
 
+  id: number;
+  name:string;
+  industry:string;
+  location:string;
+  size:string;
+  userID:number;
+
+  constructor(
+    id: number,
+    name:string,
+    industry:string,
+    location:string,
+    size:string,
+    userID:number
+  ){
+    this.id = id;
+    this.name = name;
+    this.industry = industry;
+    this.location = location;
+    this.size = size;
+    this.userID = userID;
+  }
+
 };
 
