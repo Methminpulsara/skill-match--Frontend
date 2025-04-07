f88f86fc237fb5bbc94aeeaaad9e817b2b1ffa66
Methminpulsara <methminpulsara10@gmail.com>
2025-04-07 18:18:42 +0530

    update

--- src/service/user/UserService.ts
+++ src/service/user/UserService.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import User from "../../model/User";
 
 @Injectable({
   providedIn:'root'
@@ -8,10 +7,8 @@ import User from "../../model/User";
 export default class UserService{
   constructor(private http:HttpClient){}
 
-  addCustomer(user:User){
-      return this.http.post("http://localhost:8080/api/user",user);
+  addCustomer(){
+      return this.http.post("http://localhost:8080/api/user");
   }
-
-  
 
 }
