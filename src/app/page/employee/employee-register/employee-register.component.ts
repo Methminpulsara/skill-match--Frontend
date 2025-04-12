819ac0cb2c17b0ea70fa403d0428338e0472e222
Methminpulsara <methminpulsara10@gmail.com>
2025-04-12 20:27:53 +0530

    update

--- src/app/page/employee/employee-register/employee-register.component.ts
+++ src/app/page/employee/employee-register/employee-register.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import EmployeeService from '../../../../service/EmployeeService';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-register',
-  imports: [RouterLink],
+  imports: [RouterLink,FormsModule,CommonModule],
   templateUrl: './employee-register.component.html',
   styleUrl: './employee-register.component.css'
 })
 export class EmployeeRegisterComponent {
+
+  nextPageNumber:number=1;
 
 
 
   constructor(employeeService:EmployeeService){}
 
 
-
-
+  nextPageOnAction(page:number){
+    this.nextPageNumber=page;
+  }
 
 }
