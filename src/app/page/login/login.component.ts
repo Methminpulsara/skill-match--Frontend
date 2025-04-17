a82619ad4f83af39e594d90cd831aa21b77e51be
Methminpulsara <methminpulsara10@gmail.com>
2025-04-17 21:11:25 +0530

    update

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import {  RouterLink, RouterModule } from '@angular/router';
+import User from '../../model/User';
+import { RoleType } from '../../../utils/Role';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink,RouterModule],
+  imports: [RouterModule,FormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+
+
+  email:string = "";
+  password:string = "";
+
+
+  login(){
+    console.log("clicked")
+    console.log(this.email);
+    console.log(this.password);
+    
+  }
+
+
+
+
 }
