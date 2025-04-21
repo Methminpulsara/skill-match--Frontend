3f6363d8c2eef5b469510ab19d3a396f1f256bfb
Methminpulsara <methminpulsara10@gmail.com>
2025-04-21 17:53:01 +0530

    update

--- src/app/page/employee/sikll/sikll.component.ts
+++ src/app/page/employee/sikll/sikll.component.ts
@@ -1,11 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sikll',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './sikll.component.html',
   styleUrl: './sikll.component.css'
 })
 export class SikllComponent {
+  isModalOpen = false;
 
+  skill = {
+    name: '',
+    proficiencyLevel: ''
+  };
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.skill = {
+      name: '',
+      proficiencyLevel: ''
+    };
+  }
+
+  submitSkill() {
+    console.log('Skill Submitted:', this.skill);
+    this.closeModal();
+  }
 }
