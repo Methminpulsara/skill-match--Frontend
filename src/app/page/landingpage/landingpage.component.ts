385735368c44c3722a169fbb5aae9f556b082349
Methminpulsara <methminpulsara10@gmail.com>
2025-05-07 23:56:08 +0530

    update

--- src/app/page/landingpage/landingpage.component.ts
+++ src/app/page/landingpage/landingpage.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landingpage',
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule, FormsModule],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css'
 })
 export class LandingpageComponent {
+
+ public isEmployeeModalOpen = false;
+  openEmployeeModal() {
+    this.isEmployeeModalOpen = true;
+  }
+
+  closeEmployeeModal() {
+    this.isEmployeeModalOpen = false;
+  }
+
 
 }
